1dadab87ecfd01028aa2dc42cd1ac342a1fb16a9
geekodour <hrishikeshbman@gmail.com>
2018-10-13 13:17:44 +0530

    wip

---
+++ test2.js
@@ -0,0 +1,100 @@
+const kbpgp = require('kbpgp');
+const rishi_pgp_key  = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBFuX3LIBEADRlVLWh9LvMK2Kpp/4XqmTNM6XcsIysUTtrpS2JvU9YZ/vJ4qh
+6cPXQui6ymws47KJjnrwEDGbZqXXDPru39W+ysJXy34JD0YF3Sp2eLuCeNIUc7fS
+v+bdjChybMUaEYSVm657ccEeGcqURNL6S0XxFiHGtdc3SOCCB0XRLgG6NryrnJKj
+99oVMkGEPBN++19z2lN3Bap0rJhQEtsdI66VojJLkzXmtUOZf2Wzer2CoPZa+Sxn
+Gy+TPOwkGr46Fyce/RnnPZTkawGVkDCt0l4yz5olVGXBBzRNf5RQnR7T3wTmSwrJ
+pD9K+DVJoDugldeP+MGVRII2aqXOukKrjHi8+UzEN0XTn9q3b+84761GRwcq6gUy
+xalVCQF3/3Nt2RNQPpONqOZh82/7c/uvr/oEMgjcGPPI9cuuBzuL1o3zmVHNKd3e
+sBb2C2ccghCsN/FIfA7TZV6XsZD+4LU7jTKlx0/WdHMjco1fJYabqQEkiBYE5UQJ
+3hR8waXPRjVP86xZATKZ+NxqM3TSfymUIwRpcGM1blMWk6fyKFDcv3mSoKvJBIML
+7qg47cMD6G8UWs1nyhu633q8QhoqXeOcsqbkUaB3ptoZ2DqaakHrw3xufqzsnTmH
+wD8/Q+2YHRg+tuLTUlxlo681aXSTm7N23yEOn55Qv1ZKGbWRtElGTOUMAwARAQAB
+tDpIcmlzaGlrZXNoIEJhcm1hbiAoaSBsb3ZlIGRvZ3MpIDxocmlzaGlrZXNoYm1h
+bkBnbWFpbC5jb20+iQI4BBMBAgAiBQJbl9yyAhsDBgsJCAcDAgYVCAIJCgsEFgID
+AQIeAQIXgAAKCRBz3TCUzSXxz8KEEADLlZkQp7HwuOaWJ3UBoquGZbsIRvVRJ2LO
+qgaTFtdZRwswrILYHqWjloggMbQYMuMZrngISZQNZkFvGFRm2t9StqAeREBfCt8S
+NmMG7I+VGQK4cVw5igy2ET+a2lTcMkmhMpNHjNaYfI/f1IVQCVW2nGdsfxV6fk1H
+j10ftmRFGJcUc9HPgtTv7zWjzTBKiQTYv/iaDIfpk3A+eIDX///s0/8zvnv6f8oB
+9CI5tFmcwKP0MjyTzsGeG13iOPqoa0kIq0bDtc6zwa2pF7EtMTS56YbiKWlseUJS
+7alqYPmVjQxAIYz2FXJrpc3tQ1IhrzK9KVfa9tVGIMv6KNhnstfGE42/jFsX1V6v
+AOziWMxJ5dL/Tq/jQqfXbf8ugXEry+KH8fpiTUyMzF9gXn7/ojAc8x+9yMDBtNiG
+AaH4pSVJEBsgaSyNA3cVxhCtpXwO0zvvZRh6Xf+83XSzOjLLAFSfN0XbZDcD1OgO
+P1KbFp70S1laSBwDBZx9n0Vvhkv8FcYQ6yUG1M0kaYFZWjb2E7HpAsL8FcsaLmKg
+EeiWxF/XOTTjz0j95qPNkYq2vH7gdI1taqaU+1OneRswRCoMPsQmTPlgA1WdHsiC
+HEI2XXqjqmS/w8Idv2F+d6GP9Ug19eRlxbkT8BbdRyArFQgfoWTYfLEnTgtnJGkQ
+IcPasprD87kCDQRbl9yyARAAsIPgXFa1lv8VV+iiTYy/4kB5AZ7mehOr+UZ0NVLS
+DG+OzTpkWFDru2fY2eYWDe/uHIAWih6Fr93haDiUfU5ZcJXPuktkbm//+GoAwiHF
+Jgc5rpgItJjF3ODwcfLcqR14L98pmSazV7S1cbvNFOUoRtr4NMh/dD7eP7xk6qlX
+KVMUM9gnHdkIUIfQEMmmpODQwfUQnuDiphFdCGKGHyKGUCf4u4eteLiaDC5uTO4k
+9ntz0x1p3NtcccnhmmrtBcTAMC+32PU2baEbnQ4GeN3uj/jaUb4y5/0EoDfeHAp7
+LzGR16LwG1wJ7WSL4pVVrdybsfIGcrtfv9+r9R+AHi+TU99Dujv/Mw3xWDZS/4Tq
+z7eNg/c8Po8xvK6Z0zxD6YAbQwvYhCBEIO1QxAMYSBLqiczH8puH24rROar7fP9M
+R+N8QCG5cCaZw7Yb5ndOZJc3WgOc+/7Xd1IAIfXOr9ig+itJhl3jCMJDWBC3SvnI
+/YQ0ytkLP3RclSkVcORs5+YaloLOmW9EpCh2dsaKwWS+Im6NxwOryXsOMYZhfaDQ
+Spig+QGUFEwhwk543YcusMLb0Z6KVAP5FXrfRtRoWKFlfTbXdx01JOuOShdjOrUV
+Q+3i0PgGtIBTa/maRW6v8q6jxR3+1tJIJCXK7Z2RH4cYfApX1EcW0rLdUVmZgUcL
+sacAEQEAAYkCHwQYAQIACQUCW5fcsgIbDAAKCRBz3TCUzSXxz0UOEAC4EwSsACR9
+2pVn71kYMFIag/4FBl+xXX9I18v2mYmR6hAYJKnmXcd9HxjRrOqLe/xZLpdBmsPB
+i6lFCiCcI7JqpyVs0WiPeE6T0O+0atyUjFG01mLKao3E1haPPgeRlRcprlgb51xx
+ykyae82SIUr7t/U94hcUo9FmXEbCno1vkCW7xXsEt9/1JkJGDLxmNKE6suCP/HgA
+ANDS3yK+NJx7n02i/dApemutNEWRUWnMUEXICBOQLtdD3C3MJik7YFKOgTXvaN13
+fIqDacDm8k3fQ/2s82ITNSW7Zyfrp3TmERPsH4h2JscDaMddgKPmkr2Q5chHOwOO
+4OzOAwDcd8P1M/vjQ9hIMrfX6XBYW2zx+BgTzxJLhTY9U+sHAIjpXN31R5iXC0DF
+g1i63KZfftKLx8KoTnge2btLfvhb99lP4rV8p6awyAk5wNB1ERXz+IzirbHKHgV4
+BJuBKIngzkVlD2a2/w6874vT4Kjd3F2ibhdbqE4i/rqImFhCCtVvObSsFuK+0oMQ
+KUA9hiV1ZI3RtD5CMjv+RpHbG85o2m3jMt1D425Ad9GuA3JMZti0Z3G8AJgnmQXs
+1fFCkbrl6vk+OiDkC9wFOQA3FRQ7jQeIiSmWAmLrm/plWD0t1AXVY7IKUfff1gq/
+vjqYr3w0pYCv6QPp3rcLOBIDaVNH0jzq6g==
+=brOu
+-----END PGP PUBLIC KEY BLOCK-----`
+
+var rishi;
+
+kbpgp.KeyManager.import_from_armored_pgp({
+  armored: rishi_pgp_key
+}, function(err, rishi) {
+  if (!err) {
+    console.log("rishi is loaded");
+    let ring = new kbpgp.keyring.KeyRing;
+    ring.add_key_manager(rishi);
+    const message = `-----BEGIN PGP MESSAGE-----
+Comment: https://keybase.io/download
+Version: Keybase Go 2.6.0 (linux)
+
+xA0DAAoBc90wlM0l8c8By+F0AOIAAAAA42kgbGlrZSB04m8gc2zhZWXgcADCwVwE
+AAEKABAFAlvBmR4JEHPdMJTNJfHPAACm0RAATsVvHvKx9FJ+jgCBoYNYTxNcANGq
+ZGEnJr4N5skCdpW9qEkD0Byhkx4CoFJ04M+kWbAg8dJQYMVmdhQUk6cWq6y5Hd2/
+VvgMw1TmnwALiwd7f/f71PHvDR+xCyaqo0/gvHjcalBET4/L4y4pL0vS9qTIPWE0
+dsEQhRGf6bbYvJuuZDR5tqvdmI4yjo02c1AMYBXCUwRksj0TLmca83ee/MabbQnB
+9kOzd0p1s5X4bdZ8Z+FMVylg5739VqPlWtEnJ3kNmzy/kylUXMrl3zy32z7pdI3x
+D/82tS7j8BSyuqwvUUg2FmOOlzpAAvWXszpkL/dCegFJDn05/y2T2Fbta0yDSq4e
+g2+X7iO9LqV0nS5bwEEBGPpWm2ahM7aedk0xeHvH0okHRW5tD5vlAUw9WjPNlxCw
+WqNhgS8tlLdTmuP3bCe9SkClfLDwtu7Ibso08CkVhm1mLTN+l9vJVH7eyGX7lnbq
+M92d4FWwHHQ2KwJf1BYQdv6lIja9i8FtT22OL9kJsS0tnyrD65qmcgvm7EiBwPwq
+4MKijt0ThK0W4VBLPlAKmQVzTp3GUYs+Gux1Zl8ODZHJ6PLPUFqS7474ycqT/cK7
+NRdgGFLFWwf6akeEmAj8i4wrkbcM1ZMHDSFhy8oKVptuFaIQFRKPI2Wx628UPCH3
+cGS8qMcj7sNhjDE=
+=f0CN
+-----END PGP MESSAGE-----`
+    kbpgp.unbox({keyfetch: ring, armored: message}, function(err, literals) {
+        if (err != null) {
+          return console.log("Problem: " + err);
+        } else {
+          console.log("decrypted message");
+          console.log(literals[0].toString());
+          var ds = km = null;
+          ds = literals[0].get_data_signer();
+          if (ds) { km = ds.get_key_manager(); }
+          if (km) {
+            console.log("Signed by PGP fingerprint");
+            console.log(km.get_pgp_fingerprint().toString('hex'));
+          }
+        }
+    });
+
+
+  }
+});
